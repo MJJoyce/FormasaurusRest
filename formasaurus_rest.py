1f4acb3e2ccf8a96d575992fb6c292f7b67c1186
Michael Joyce <mltjoyce@gmail.com>
2015-11-13 18:30:42 -0800

    Clean up duplicate code

--- formasaurus_rest.py
+++ formasaurus_rest.py
@@ -20,16 +20,14 @@ def hello():
 def extract_local_file_forms(file_path):
     full_path = os.path.join(LEADER, file_path)
     tree = lxml.html.parse(full_path)
-    forms = ex.extract_forms(tree)
-    forms = [
-        (lxml.etree.tostring(form[0]), form[1], tree.getpath(form[0]))
-        for form in forms
-    ]
-    return json.dumps(forms)
+    return _process_parsed_tree(tree)
 
 @app.route("/extract/url/<path:url>")
 def extract_site_forms(url):
     tree = lxml.html.parse(url)
+    return _process_parsed_tree(tree)
+
+def _process_parsed_tree(tree):
     forms = ex.extract_forms(tree)
     forms = [
         (lxml.etree.tostring(form[0]), form[1], tree.getpath(form[0]))
